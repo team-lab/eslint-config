fd20ec2a1eca14df9ca5e6404533c3f202d31d82
tamuraryoya <tamuraryoya@team-lab.com>
2019-06-16 20:51:30 +0900

    ⚡️ Add TypeScript parser options

--- typescript.js
+++ typescript.js
@@ -1,3 +1,12 @@
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: false
+    }
+  },
   extends: ["./rules/base.yml", "./rules/typesript.yml"]
 };
